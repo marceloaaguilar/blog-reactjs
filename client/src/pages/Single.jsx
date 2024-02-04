be6afc1c18f8e7b03106d4b734a430e4ff5e6727
Marcelo Aguilar <marcelodalmeida42@gmail.com>
2024-02-04 14:20:59 -0300

    Update

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Edit from "../images/editar.png"
+import Delete from "../images/excluir.png"
+import Menu from "../components/Menu";
 const Single = () => {
 
     return (
@@ -15,13 +17,21 @@ const Single = () => {
                 </div>
                 <div className="edit">
                     <Link to={`/write?edit=2`}>
-                        <svg fill="#000000" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 348.882 348.882" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M333.988,11.758l-0.42-0.383C325.538,4.04,315.129,0,304.258,0c-12.187,0-23.888,5.159-32.104,14.153L116.803,184.231 c-1.416,1.55-2.49,3.379-3.154,5.37l-18.267,54.762c-2.112,6.331-1.052,13.333,2.835,18.729c3.918,5.438,10.23,8.685,16.886,8.685 c0,0,0.001,0,0.001,0c2.879,0,5.693-0.592,8.362-1.76l52.89-23.138c1.923-0.841,3.648-2.076,5.063-3.626L336.771,73.176 C352.937,55.479,351.69,27.929,333.988,11.758z M130.381,234.247l10.719-32.134l0.904-0.99l20.316,18.556l-0.904,0.99 L130.381,234.247z M314.621,52.943L182.553,197.53l-20.316-18.556L294.305,34.386c2.583-2.828,6.118-4.386,9.954-4.386 c3.365,0,6.588,1.252,9.082,3.53l0.419,0.383C319.244,38.922,319.63,47.459,314.621,52.943z"></path> <path d="M303.85,138.388c-8.284,0-15,6.716-15,15v127.347c0,21.034-17.113,38.147-38.147,38.147H68.904 c-21.035,0-38.147-17.113-38.147-38.147V100.413c0-21.034,17.113-38.147,38.147-38.147h131.587c8.284,0,15-6.716,15-15 s-6.716-15-15-15H68.904c-37.577,0-68.147,30.571-68.147,68.147v180.321c0,37.576,30.571,68.147,68.147,68.147h181.798 c37.576,0,68.147-30.571,68.147-68.147V153.388C318.85,145.104,312.134,138.388,303.85,138.388z"></path> </g> </g></svg>
-                        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M18 6L17.1991 18.0129C17.129 19.065 17.0939 19.5911 16.8667 19.99C16.6666 20.3412 16.3648 20.6235 16.0011 20.7998C15.588 21 15.0607 21 14.0062 21H9.99377C8.93927 21 8.41202 21 7.99889 20.7998C7.63517 20.6235 7.33339 20.3412 7.13332 19.99C6.90607 19.5911 6.871 19.065 6.80086 18.0129L6 6M4 6H20M16 6L15.7294 5.18807C15.4671 4.40125 15.3359 4.00784 15.0927 3.71698C14.8779 3.46013 14.6021 3.26132 14.2905 3.13878C13.9376 3 13.523 3 12.6936 3H11.3064C10.477 3 10.0624 3 9.70951 3.13878C9.39792 3.26132 9.12208 3.46013 8.90729 3.71698C8.66405 4.00784 8.53292 4.40125 8.27064 5.18807L8 6M14 10V17M10 10V17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                        <img src={Edit} />
                     </Link>
+                        <img src={Delete} alt="" srcset="" />
                 </div>
             </div>
+            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio recusandae ex non? Fuga, unde consequuntur perferendis nostrum facilis impedit quibusdam nihil laborum! Enim neque sunt autem, explicabo exercitationem distinctio perspiciatis.</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum quasi corrupti eum debitis quod! Vero, voluptate dolores tempore tenetur iusto perspiciatis fugiat ex! Numquam vel blanditiis, cum doloribus beatae tempore.
+            Laboriosam repellendus maiores voluptates vero quisquam blanditiis. Doloribus aliquid voluptate inventore nostrum fuga repudiandae quo, corporis animi blanditiis a ad amet reiciendis sint modi! Reiciendis reprehenderit illo veniam a repellendus!
+            Nemo at optio architecto quam dolorem temporibus non, voluptates animi quaerat, incidunt eius ducimus vitae repudiandae ex nesciunt esse minima omnis. Delectus vitae odio tenetur eaque. Ratione minus ipsam suscipit.
+            Cupiditate in voluptates eveniet non blanditiis voluptatem, aliquid delectus. Facilis, aut. Laboriosam error, fugit amet molestias eius, nisi minus, pariatur dolor quod ullam mollitia voluptas reprehenderit aliquid sed officiis tenetur.
+            Unde cum hic eligendi temporibus distinctio. Veritatis eos cumque iusto blanditiis porro provident. Laboriosam quia praesentium dolore beatae quaerat temporibus et nam. Ullam, sapiente iusto numquam nemo rerum expedita nam.
+            Quae eveniet, voluptate eos distinctio sed saepe ipsam, sit quos eius eaque amet delectus, ratione officia consequatur soluta et quam rem neque praesentium sunt. Illum quae odio id excepturi neque.</p>
+        
         </div>
-        <div className="menu">m</div>
+        <Menu/>
     </div>)
     
 }
