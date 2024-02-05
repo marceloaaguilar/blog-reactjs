7d14a6f2aef281e259bf839080a354166c0eb415
marceloaaguilar <marcelodalmeida42@gmail.com.br>
2024-02-05 13:32:24 -0300

    Updates

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from 'react-router-dom'
 
 
 
 const Register = () => {
 
+
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+        password: "",
+    })
+
+    const handlerChange = e =>{
+        setInputs(prev=> ({...prev, [e.target.name]: e.target.value}))
+    }
+
     return (
         <div className="auth">
             <h1>Register</h1>
             <form action="">
-                <input required type="text" name="username" placeholder="Username" />
-                <input required type="email" name="email" placeholder="E-mail" />
-                <input required type="password" name="password" placeholder="Password"/>
+                <input required type="text" name="username" onChange={handlerChange} placeholder="Username" />
+                <input required type="email" name="email" onChange={handlerChange} placeholder="E-mail" />
+                <input required type="password" name="password" onChange={handlerChange} placeholder="Password"/>
                 <button type="submit">Login</button>
                 <p>Error!</p>
                 <span>Do you have an account? <Link to="/login">Login</Link></span>
